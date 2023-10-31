9ff4d4f583707b391599beac8c78fd4712249d76
112310247 <146065161+112310247@users.noreply.github.com>
2023-10-31 15:11:01 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int time;
+    double money;
+    scanf("%d",&time);
+    if(time<=800)
+    {
+        money=time*0.9;
+        money=(int)(money*1000+0.5)/1000.0;
+        printf("%0.1f",money);
+    }
+    if(time>800&&time<1500)
+    {
+        money=time*0.81;
+        money=(int)(money*1000+0.5)/1000.0;
+        printf("%0.1f",money);
+    }
+    if(time>=1500)
+    {
+        money=time*0.79;
+        money=(int)(money*1000+0.5)/1000.0;
+        printf("%0.1f",money);
+    }
+}
