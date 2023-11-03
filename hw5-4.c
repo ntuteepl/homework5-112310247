d73e8dc530901f7a96d6e23c731300053361fcb4
112310247 <146065161+112310247@users.noreply.github.com>
2023-11-03 21:16:59 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int H, M;
+    float h, m, t;
+    scanf("%d %d", &H, &M);
+
+    if (H <= 12 && H >= 1 && M >= 0 && M <= 59) {
+        h = H * 30 + 0.5 * M;
+        m = 6 * M;
+        t = h - m;
+
+        if (t > 180) {
+            t = t - 180;
+        } else if (t < -180) {
+            t = t + 180;
+        } else if (t < 0) {
+            t = -t;
+        }
+        printf("%0.3f",t);
+    } else {
+        printf("輸入的時間不符合條件\n");
+    }
+
+    return 0;
+}
