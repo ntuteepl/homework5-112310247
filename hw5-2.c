bd1b08a518c18654a786535aaaceec0c718fe388
112310247 <146065161+112310247@users.noreply.github.com>
2023-11-03 21:25:14 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-int main()
-{
-    int time;
-    double money;
-    scanf("%d",&time);
-    if(time<=800)
-    {
-        money=time*0.9;
-        money=(int)(money*1000+0.5)/1000.0;
-        printf("%0.1f",money);
+int main() {
+    int minutes;
+    double cost;
+
+    printf("請輸入通話分鐘數：");
+    scanf("%d", &minutes);
+
+    if (minutes <= 800) {
+        cost = minutes * 0.9;
+    } else if (minutes > 800 && minutes <= 1500) {
+        cost = 800 * 0.9 + (minutes - 800) * 0.9 * 0.9;
+    } else {
+        cost = 800 * 0.9 + 700 * 0.9 * 0.9 + (minutes - 1500) * 0.9 * 0.79;
     }
-    if(time>800&&time<1500)
-    {
-        money=time*0.81;
-        money=(int)(money*1000+0.5)/1000.0;
-        printf("%0.1f",money);
-    }
-    if(time>=1500)
-    {
-        money=time*0.79;
-        money=(int)(money*1000+0.5)/1000.0;
-        printf("%0.1f",money);
-    }
+
+    // 四捨五入到小數點以下第一位
+    cost = round(cost * 10) / 10;
+
+    printf("通話費用為：%.1f\n", cost);
+
+    return 0;
 }
+
